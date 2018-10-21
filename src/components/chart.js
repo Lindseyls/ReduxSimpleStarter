57cea935f8038d78024480c2c03bd44cff0df099
Luxi <luxi.lindsey@gmail.com>
2018-10-21 00:17:45 -0700

    Added labeling of units and a reference line to the sparkline graph.

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
 
-export default ({ data, color }) => {
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
+
+export default ({ data, color, units }) => {
 
   return (
     <div>
-      <Sparklines height={120} width={180} data={data}>
+      <Sparklines svgHeight={150} svgWidth={220} data={data}>
         <SparklinesLine color={color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(data)} {units}</div>
     </div>
   )
 }
